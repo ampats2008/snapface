8e0ba75aa51cc9e14b6af32b10429ada63fd7e2a
Anthony M <ampats2008@gmail.com>
2022-04-18 14:02:06 -0400

    added components / state to UserProfile page

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,25 +1,26 @@
 import type { NextPage } from 'next'
-import Image from 'next/image'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import { LoginButton } from '../../components/login/LoginButton'
-import { userQuery, useUser } from '../../hooks/useUser'
-import { client } from '../../sanity-scripts/client'
+import { useUser } from '../../hooks/useUser'
+import { Loading, ProfileBanner, ProfilePostsFilter } from '../../components'
+import { useEffect, useState } from 'react'
 
 // Depends on User to be signed in:
 const UserProfile: NextPage = () => {
   const [user] = useUser()
 
-  if (!user) return <div>Page loading...</div>
+  useEffect(() => {
+    console.log(user)
+  }, [user])
+
+  // Call for posts based on filter value
+  const [filter, setFilter] = useState('myPosts')
+
+  if (!user) return <Loading />
 
   return (
-    <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-      <div className='flex items-center'>
-        <Image width="60px" height="60px" className='inline-block' src={user.profileImg} />
-        <h1 className="ml-4 text-4xl font-bold inline-block">
-          {user.firstName} {user.lastName}
-        </h1>
-      </div>
+    <main>
+      <ProfileBanner {...{ user }} />
+      {/* Posts feed with controls for filtering by *liked* and *postedBy* current user */}
+      <ProfilePostsFilter {...{setFilter, filter}} />
     </main>
   )
 }
