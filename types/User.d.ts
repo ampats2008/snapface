22a98dc3b78585e55102a786b06c17d6e99db302
Anthony M <ampats2008@gmail.com>
2022-04-18 14:02:19 -0400

    added props to User type

--- types/User.d.ts
+++ types/User.d.ts
@@ -1,11 +1,14 @@
 export type User = {
-    _createdAt: Date;
-    _id:        string;
-    _rev:       string;
-    _type:      string;
-    _updatedAt: Date;
-    email:      string;
-    firstName:  string;
-    lastName:   string;
-    profileImg: string;
+  _createdAt: Date
+  _id: string
+  _rev: string
+  _type: string
+  _updatedAt: Date
+  email: string
+  firstName: string
+  lastName: string
+  profileImg: string
+  bio?: string
+  profileBanner?: string // might change this to sanity-image
+  userName?: string
 }
