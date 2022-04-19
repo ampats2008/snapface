e7e897120ce8719d5894ed643c96ac76c4ff1c2d
Anthony M <ampats2008@gmail.com>
2022-04-18 21:35:36 -0400

    renamed component

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
 // allows me to import multiple components on one line in other files
-export { default as LandingContent } from './home/LandingContent'
-export { default as UserLanding } from './home/UserLanding'
+export { default as LoggedOutLanding } from './home/LoggedOutLanding'
+export { default as UserLanding } from './user/UserLanding'
 export { default as StyledButton } from './StyledButton'
 export { default as Loading } from './Loading'
 export { default as ProfileBanner } from './user/ProfileBanner'
