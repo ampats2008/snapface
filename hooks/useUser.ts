f0cc4d6fa3ae778803c5695d3b737b5c89d965d3
Anthony M <ampats2008@gmail.com>
2022-04-18 21:35:10 -0400

    improved useUser - returns loading / loggedIn states with user

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,30 +1,52 @@
-import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { useState, useEffect, useMemo } from 'react'
+import { GoogleLoginResponse } from 'react-google-login'
 import { client } from '../sanity-scripts/client'
 import { User } from '../types/User'
 
-export const useUser: () => [User | null] = () => {
+export const useUser: () => {
+  user: User | null
+  isLoading: boolean
+  isLoggedIn: boolean
+} = () => {
   const [user, setUser] = useState<User | null>(null)
 
-  // might need to rework this
-  // Sign in button doesn't disappear after first time sign in
-  // have to refresh first
+  const router = useRouter() // used to redirect user if their account could not be found in our DB
+
+  // Get current user from window.localStorage
+  const localUser =
+    typeof window !== 'undefined' ? localStorage.getItem('user') : undefined
+
+  const userInfo: GoogleLoginResponse | undefined = useMemo(() => {
+    // if localUser is defined, parse the string and return it as userInfo, else return undefined.
+    if (typeof localUser === 'undefined' || localUser === null) {
+      if (typeof window !== 'undefined') localStorage.clear()
+      return
+    }
+    return JSON.parse(localUser)
+  }, [localUser]) // only recompute val if localUser changes
+
   useEffect(() => {
-    const localUser = localStorage.getItem('user')
+    if (typeof userInfo !== 'undefined' && userInfo !== null) {
+      // when userInfo is defined, query Sanity for the user's info.
+      const query = userQuery(userInfo?.googleId)
 
-    const userInfo =
-      localUser !== undefined && localUser !== null
-        ? JSON.parse(localUser)
-        : localStorage.clear()
+      client
+        .fetch(query)
+        .then((data) => setUser(data[0]))
+        .catch(() => {
+          // if user account could not be found, notify the user and redirect them back to the login screen
+          alert(
+            "Sorry, we couldn't find your account at this time. Please try to sign in again."
+          )
+          router.replace('/login')
+        })
+    }
+  }, [userInfo])
 
-    const query = userQuery(userInfo?.googleId)
+  const isLoggedIn = typeof userInfo !== 'undefined'
 
-    client
-      .fetch(query)
-      .then((data) => setUser(data[0]))
-      .catch((err) => console.log(err))
-  }, [])
-
-  return [user]
+  return { user, isLoading: !user && isLoggedIn ? true : false, isLoggedIn }
 }
 
 // query builder helper
