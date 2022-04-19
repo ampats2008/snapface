1934e10b937338fabb284fce0c99ffef17f7b49c
Anthony M <ampats2008@gmail.com>
2022-04-19 12:38:10 -0400

    added sidebar to discover layout -- wip

--- pages/discover/index.tsx
+++ pages/discover/index.tsx
@@ -1,13 +1,94 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 const Discover: NextPage = () => {
+  const [category, setCategory] = useState<string | null>(null)
+
+  useEffect(() => {
+    console.log(category)
+  }, [category])
+
   return (
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-4xl font-bold">Discover new posts...</h1>
-      </main>
+    <main className="py-10 px-4 xl:p-10">
+      <h1 className="mb-10 text-4xl font-bold">
+        Find something cool to share...
+      </h1>
+      <div id="dashboard" className="grid-cols-[200px,_1fr] sm:grid">
+        <aside className="mb-10">
+          <h2 className="mb-4 font-bold">Categories:</h2>
+          <Category
+            title={'technology'}
+            onClick={() => setCategory('technology')}
+            isActive={category === 'technology' ? true : false}
+          />
+          <Category
+            title={'nature'}
+            onClick={() => setCategory('nature')}
+            isActive={category === 'nature' ? true : false}
+          />
+          <Category
+            title={'sports'}
+            onClick={() => setCategory('sports')}
+            isActive={category === 'sports' ? true : false}
+          />
+          <Category
+            title={'gaming'}
+            onClick={() => setCategory('gaming')}
+            isActive={category === 'gaming' ? true : false}
+          />
+          <Category
+            title={'art'}
+            onClick={() => setCategory('art')}
+            isActive={category === 'art' ? true : false}
+          />
+        </aside>
+
+        {/* feed takes category as prop */}
+        <section className="mb-10">
+          <h2 className="ml-10 mb-4 font-bold">Feed:</h2>
+        </section>
+      </div>
+    </main>
   )
 }
 
 export default Discover
+
+// Category component
+const Category = ({
+  isActive,
+  onClick,
+  title,
+}: {
+  isActive: boolean
+  onClick: React.MouseEventHandler
+  title: string
+}) => {
+  const bottomBColor = isActive
+    ? 'border-brand-600'
+    : 'border-gray-300 hover:border-brand-300'
+
+  return (
+    <div
+      id="categoryBtn"
+      onClick={onClick}
+      className={`block cursor-pointer border-b-2 p-4 transition-all xl:border-b-0 xl:border-r-2 ${bottomBColor}`}
+    >
+      <div
+        id="categoryInfoGroup"
+        className="mx-auto flex w-max items-center sm:mx-0"
+      >
+        <Image
+          className="rounded-full"
+          width={'50px'}
+          height={'50px'}
+          src={`https://source.unsplash.com/100x100/?${title}`}
+        />
+        <p className="ml-3 capitalize">{title}</p>
+      </div>
+    </div>
+  )
+}
--- pages/user/welcome.tsx
+++ pages/user/welcome.tsx
@@ -1,23 +1,26 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { Loading, UserLanding } from '../../components'
 import { useUser } from '../../hooks/useUser'
 
 const UserMenu: NextPage = () => {
+  const { user, isLoading, isLoggedIn } = useUser() // get the current user
   
-  const { user, isLoading , isLoggedIn } = useUser() // get the current user
+  const router = useRouter()
 
   useEffect(() => {
     // scroll to top on mount
-    window.scrollTo(0,0)
+    window.scrollTo(0, 0)
     // localStorage.clear()
   }, [])
 
   if (isLoading) return <Loading />
 
+  if (!isLoggedIn) router.push('/')
+
   return (
     <main className="grid grid-rows-[25vh_1fr] place-items-center">
-      {/* display only if not logged in: */}
       <UserLanding user={user!} />
     </main>
   )
