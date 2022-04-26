d3c1afae326210867c07af8494edea231f89322c
Anthony M <ampats2008@gmail.com>
2022-04-26 13:10:35 -0400

    formatted with prettier

--- components/user/ProfileBanner.tsx
+++ components/user/ProfileBanner.tsx
@@ -11,7 +11,7 @@ const ProfileBanner = ({ user }: Props) => {
 
   return (
     <section
-      className={`grid place-content-center bg-black p-4 text-[#f5f5f5] xl:min-h-[350px] xl:p-5`}
+      className={`grid place-content-center bg-black py-10 px-6 text-[#f5f5f5] sm:p-28 `}
       style={{
         backgroundImage: `radial-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.7)), url('${randomBannerUrl}')`,
         backgroundPosition: 'top center, center 40%',
@@ -24,16 +24,26 @@ const ProfileBanner = ({ user }: Props) => {
           width="70px"
           height="70px"
           className="inline-block rounded-full border-4 border-white shadow-sm"
-          src={(user.profileImg) ? user.profileImg : 'https://source.unsplash.com/70x70/?nature,photography,technology'}
+          src={
+            user.profileImg
+              ? user.profileImg
+              : 'https://source.unsplash.com/70x70/?nature,photography,technology'
+          }
         />
         <div className="ml-4">
           <h1 className=" text-4xl font-bold">
             {user.firstName} {user.lastName}
           </h1>
-          {(user.userName) && <p className="mt-1 text-sm text-gray-300">@{user.userName}</p>}
+          {user.userName && (
+            <p className="mt-1 text-sm text-gray-300">@{user.userName}</p>
+          )}
         </div>
       </div>
-      <h3 className="mt-5 max-w-[50ch] leading-[1.7]">{(user.bio) ? user.bio : "You haven't created a bio yet. Edit your profile to add one."}</h3>
+      <h3 className="mt-5 max-w-[50ch] leading-[1.7]">
+        {user.bio
+          ? user.bio
+          : "You haven't created a bio yet. Edit your profile to add one."}
+      </h3>
     </section>
   )
 }
