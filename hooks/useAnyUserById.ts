99876b05063d51d755d4bfe7cb60c0e9f7160bb1
Anthony M <ampats2008@gmail.com>
2022-05-03 13:03:29 -0400

    added optional params

--- hooks/useAnyUserById.ts
+++ hooks/useAnyUserById.ts
@@ -3,13 +3,20 @@ import { client } from '../sanity-scripts/client'
 import { User } from '../types/User'
 import { userQuery } from './useUser'
 
-export const useAnyUserById = (userID: User['_id'] | undefined) => {
+export const useAnyUserById = (
+  userID: User['_id'] | undefined,
+  initialData?: User
+) => {
   const {
     data: user,
     isLoading,
     isError,
     error,
-  } = useQuery<User>(['getUserById', userID], () => client.fetch(userQuery(userID)))
+  } = useQuery<User>(
+    ['getUserById', userID],
+    () => client.fetch(userQuery(userID)),
+    { initialData }
+  )
 
   return {
     user,
--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,18 +1,22 @@
 import { useState, useEffect, useMemo } from 'react'
-import { useQuery } from 'react-query'
+import { QueryKey, useQuery, UseQueryOptions } from 'react-query'
 import { client } from '../sanity-scripts/client'
 import { Session } from 'next-auth/core/types'
 import { User } from '../types/User'
 
 export const useUser: (
   session: Session | null,
-  status: string
+  status: string,
+  options?: Omit<
+    UseQueryOptions<User, unknown, User, QueryKey>,
+    'queryFn' | 'queryKey'
+  >
 ) => {
   user: User | undefined
   isLoading: boolean
   isError: boolean
   error: any
-} = (session, status) => {
+} = (session, status, options) => {
   // if session status === authenticated, call for user data from sanity.
 
   const {
@@ -23,7 +27,11 @@ export const useUser: (
   } = useQuery<User>(
     ['getUser', session, status],
     () => client.fetch(userQuery(session!.user.id)),
-    { enabled: status === 'authenticated', refetchOnWindowFocus: false }
+    {
+      enabled: status === 'authenticated',
+      refetchOnWindowFocus: false,
+      ...options,
+    }
   )
 
   return { user, isLoading, isError, error }
