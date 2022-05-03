3036b34214210a3fba96508f357ae3378986d123
Anthony M <ampats2008@gmail.com>
2022-05-03 13:05:50 -0400

    added client side rendering in case data needs to be refetched while user is looking at page

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,16 +1,29 @@
 import type { NextPage, NextPageContext } from 'next'
-import { Feed, ProfileBanner, ProfilePostsFilter } from '../../components'
+import {
+  Feed,
+  Loading,
+  ProfileBanner,
+  ProfilePostsFilter,
+} from '../../components'
 import { useState } from 'react'
 import { client } from '../../sanity-scripts/client'
 import { User } from '../../types/User'
+import { useAnyUserById } from '../../hooks/useAnyUserById'
+import Error from 'next/error'
 
 // Private version of profile page: requires user to be signed in:
 // the public user profile page will be a SSR page that doesn't interface with useUser / useSession
-const UserProfile: NextPage<{ initialData: User }> = ({
-  initialData: pageUser,
-}) => {
+const UserProfile: NextPage<{ initialData: User }> = ({ initialData }) => {
+  const {
+    user: pageUser,
+    isLoading,
+    isError,
+  } = useAnyUserById(initialData?._id, initialData)
   // Call for posts based on filter value
   const [filter, setFilter] = useState<'myPosts' | 'myLikedPosts'>('myPosts')
+
+  if (isLoading) return <Loading />
+  if (isError) return <Error statusCode={401} />
 
   return (
     <main className="mt-4 xl:mt-0">
@@ -18,7 +31,7 @@ const UserProfile: NextPage<{ initialData: User }> = ({
       {/* Posts feed with controls for filtering by *liked* and *postedBy* current user */}
       <ProfilePostsFilter {...{ setFilter, filter }} />
       <section className="mb-10 xl:mx-auto xl:w-[80vw]">
-        <Feed filterByServer={filter} userId={pageUser._id} />
+        {pageUser && <Feed filterByServer={filter} userId={pageUser._id} />}
       </section>
     </main>
   )
@@ -30,6 +43,7 @@ export async function getServerSideProps(context: NextPageContext) {
   const query = `*[_type == 'user' && _id == '${context.query.id}'][0]`
 
   const initialData = await client.fetch(query)
+  console.log(initialData)
 
   // ?: if this is only being used as prefetch data,
   // ?: do you still need to handle errors?
